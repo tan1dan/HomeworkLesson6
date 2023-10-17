8dc1c696c51692a76973cd802a5f97dd82d3f50c
Ivan Znak <ivanznak04@gmail.com>
2023-10-17 17:53:12 +0200

    Initial commit

---
+++ HomeworkLesson6/HomeworkLesson6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeworkLesson6
+//
+//  Created by Иван Знак on 17/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
