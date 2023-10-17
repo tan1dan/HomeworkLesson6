0247ed92fa1a5f6b1a85e60922ba57193f4f2f73
Ivan Znak <ivanznak04@gmail.com>
2023-10-17 18:06:28 +0200

    zadanie1 was added

--- HomeworkLesson6/HomeworkLesson6/ViewController.swift
+++ HomeworkLesson6/HomeworkLesson6/ViewController.swift
@@ -11,7 +11,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let array = ["abc", "dwefgefg13", "oiut", "qwertfds", "3fdvdsfvv", "qwef gsdjkfnb", "qegbg sdg qwe"]
         
+        //zadanie1
+        
+        let countOfArray = array.flatMap{ element in element}.count
+        print(countOfArray)
+        
+        var count = 0
+        for element in array{
+            for _ in element{
+                count += 1
+            }
+        }
+        print(count)
     }
 
 
